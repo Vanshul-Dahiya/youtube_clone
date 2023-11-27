645d78cc431c2a5fe2b861cbbb42a4695ff68d7e
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2023-11-27 08:51:16 +0530

    rendered videos on feed

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import { VideoCard, ChannelCard } from "./";
 
-const Videos = () => {
+const Videos = ({ videos }) => {
   return (
-    <div>Videos</div>
-  )
-}
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
-export default Videos
+export default Videos;
